d690cc997a198d960c621abf09ccfc719057f7a1
zhangkaili111 <13777486302@163.com>
2019-08-01 14:59:11 +0800

    commit settings_testing.py first

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'YtX#_]O[6n',                        # 你的数据库password
-        'HOST': '10.1.0.16',                   		   # 数据库HOST
+        'HOST': '172.30.16.2',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
